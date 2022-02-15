e69b2db2bedf29b656c25ecb74dbabccf539ec57
ranshibo <195523241@qq.com>
2022-02-15 17:25:19 +0800

    init project

---
+++ rapis/src/main/java/com/rapis/RapisApplication.java
@@ -0,0 +1,13 @@
+package com.rapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapisApplication.class, args);
+	}
+
+}
---
+++ rapis/src/test/java/com/rapis/RapisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
